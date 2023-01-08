b07704e3d1e08cc8d8d62e78b82a124dd60f01f1
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2023-01-08 18:41:36 +0200

    Update workflow.py

--- workflow.py
+++ workflow.py
@@ -50,3 +50,51 @@ def kfpipeline():
     step_1_7 = funcs['func-1'].as_step(params = {"x" : step_1_6.outputs['return']},outputs = ['return'])
     
     step_1_8 = funcs['func-1'].as_step(params = {"x" : step_1_7.outputs['return']},outputs = ['return'])
+    
+    step_2_1 = funcs['func-1'].as_step(params = {"x" : 0},outputs = ['return'])
+    
+    step_2_2 = funcs['func-1'].as_step(params = {"x" : step_2_1.outputs['return']},outputs = ['return'])
+    
+    step_2_3 = funcs['func-1'].as_step(params = {"x" : step_2_2.outputs['return']},outputs = ['return'])
+    
+    step_2_4 = funcs['func-1'].as_step(params = {"x" : step_2_3.outputs['return']},outputs = ['return'])
+    
+    step_2_5 = funcs['func-1'].as_step(params = {"x" : step_2_4.outputs['return']},outputs = ['return'])
+    
+    step_2_6 = funcs['func-1'].as_step(params = {"x" : step_2_5.outputs['return']},outputs = ['return'])
+    
+    step_2_7 = funcs['func-1'].as_step(params = {"x" : step_2_6.outputs['return']},outputs = ['return'])
+    
+    step_2_8 = funcs['func-1'].as_step(params = {"x" : step_2_7.outputs['return']},outputs = ['return'])
+    
+    step_3_1 = funcs['func-1'].as_step(params = {"x" : 0},outputs = ['return'])
+    
+    step_3_2 = funcs['func-1'].as_step(params = {"x" : step_3_1.outputs['return']},outputs = ['return'])
+    
+    step_3_3 = funcs['func-1'].as_step(params = {"x" : step_3_2.outputs['return']},outputs = ['return'])
+    
+    step_3_4 = funcs['func-1'].as_step(params = {"x" : step_3_3.outputs['return']},outputs = ['return'])
+    
+    step_3_5 = funcs['func-1'].as_step(params = {"x" : step_3_4.outputs['return']},outputs = ['return'])
+    
+    step_3_6 = funcs['func-1'].as_step(params = {"x" : step_3_5.outputs['return']},outputs = ['return'])
+    
+    step_3_7 = funcs['func-1'].as_step(params = {"x" : step_3_6.outputs['return']},outputs = ['return'])
+    
+    step_3_8 = funcs['func-1'].as_step(params = {"x" : step_3_7.outputs['return']},outputs = ['return'])
+    
+    step_4_1 = funcs['func-1'].as_step(params = {"x" : 0},outputs = ['return'])
+    
+    step_4_2 = funcs['func-1'].as_step(params = {"x" : step_4_1.outputs['return']},outputs = ['return'])
+    
+    step_4_3 = funcs['func-1'].as_step(params = {"x" : step_4_2.outputs['return']},outputs = ['return'])
+    
+    step_4_4 = funcs['func-1'].as_step(params = {"x" : step_4_3.outputs['return']},outputs = ['return'])
+    
+    step_4_5 = funcs['func-1'].as_step(params = {"x" : step_4_4.outputs['return']},outputs = ['return'])
+    
+    step_4_6 = funcs['func-1'].as_step(params = {"x" : step_4_5.outputs['return']},outputs = ['return'])
+    
+    step_4_7 = funcs['func-1'].as_step(params = {"x" : step_4_6.outputs['return']},outputs = ['return'])
+    
+    step_4_8 = funcs['func-1'].as_step(params = {"x" : step_4_7.outputs['return']},outputs = ['return'])
