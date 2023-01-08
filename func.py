1325e47d160cc1bca081142a6f0d2b6e64070456
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2023-01-08 18:33:00 +0200

    Add files via upload

---
+++ func.py
@@ -0,0 +1,7 @@
+import time
+
+def func(x):
+    for i in range(100000):
+        context.logger.info("salkfhalkscvnlaknvioasfqw70192745091241!@5!55&JKBFCFBXCVSDasklfalskjc,mkansklcnccnncnncne8383")
+        time.sleep(0.003)
+        return 1
