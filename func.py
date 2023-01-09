f48d07e4035bc9a283468c65c1fb768021539d5a
GiladShapira94 <100074049+GiladShapira94@users.noreply.github.com>
2023-01-09 09:44:49 +0200

    Update func.py

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 import time
 
 def func(context,x):
-    for i in range(100000):
+    for i in range(1000):
         context.logger.info("salkfhalkscvnlaknvioasfqw70192745091241!@5!55&JKBFCFBXCVSDasklfalskjc,mkansklcnccnncnncne8383")
         time.sleep(0.003)
     return 1
